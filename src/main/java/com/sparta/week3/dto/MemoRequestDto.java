4a87d275581936fa7ebb5f4cb28c24629c004b21
RefinedStone <chem.en.9273@gmail.com>
2022-10-03 01:37:43 +0900

    커밋문제로 다시 커밋합니다

--- src/main/java/com/sparta/week3/dto/MemoRequestDto.java
+++ src/main/java/com/sparta/week3/dto/MemoRequestDto.java
@@ -1,2 +1,14 @@
-package com.sparta.week3.dto;public class MemoRequestDto {
+package com.sparta.week3.dto;
+
+import com.sparta.week3.domain.Timestamped;
+import lombok.Getter;
+
+@Getter
+public class MemoRequestDto extends Timestamped {
+    private String username;
+    private String contents;
+    private String password;
+
+
+
 }
--- src/main/java/com/sparta/week3/repository/MemoRepository.java
+++ src/main/java/com/sparta/week3/repository/MemoRepository.java
@@ -1,2 +1,10 @@
-package com.sparta.week3.repository;public class MemoRepository {
+package com.sparta.week3.repository;
+
+import com.sparta.week3.domain.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findAllByOrderByModifiedAtDesc();
 }
