1f9cfe7d80657fb7f7d215b904ca8886e595f0ed
RefinedStone <chem.en.9273@gmail.com>
2022-10-10 23:53:45 +0900

    1-11까지 강의 내용 업로드 하였습니다

--- src/main/java/com/sparata/lec01/controller/HomeController.java
+++ src/main/java/com/sparata/lec01/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.sparata.lec01.controller;
 
 
+import com.sparata.lec01.model.UserRoleEnum;
 import com.sparata.lec01.security.UserDetailsImpl;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,9 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         model.addAttribute("username", userDetails.getUsername());
+        if (userDetails.getUser().getRole() == UserRoleEnum.ADMIN) {
+            model.addAttribute("admin_role", true);
+        }
         return "index";
     }
 }
