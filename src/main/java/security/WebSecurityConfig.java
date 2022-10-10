270d57dcece7d542c8c62389aa318dd051d68a1f
RefinedStone <chem.en.9273@gmail.com>
2022-10-10 15:49:45 +0900

    스프링 시큐리티 페이지 코드 작성 기본 프레임 입니다

---
+++ src/main/java/security/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        // 어떤 요청이든 '인증'
+                        .anyRequest().authenticated()
+                )
+                // 로그인 기능 허용
+                .formLogin()
+                .loginPage("/user/login") // 사용자 정의 페이지 경로
+                .defaultSuccessUrl("/") // 로그인 성공 시 이동 페이지 경로
+                .failureUrl("/user/login?error") // 로그인 실패 시 이동 페이지 경로
+                .permitAll();
+
+        return http.build();
+    }
+}
