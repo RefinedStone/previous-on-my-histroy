768a6260c218e104d9db8821989fce2e366df662
RefinedStone <chem.en.9273@gmail.com>
2022-10-10 20:43:30 +0900

    1-10 까지 강의 내용 코드 작성 하였습니다

--- src/main/java/com/sparata/lec01/security/WebSecurityConfig.java
+++ src/main/java/com/sparata/lec01/security/WebSecurityConfig.java
@@ -1,13 +1,12 @@
 package com.sparata.lec01.security;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.web.SecurityFilterChain;
-
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
@@ -42,15 +41,22 @@ public class WebSecurityConfig {
                         // 어떤 요청이든 '인증'
                         .anyRequest().authenticated()
                 )
-                // 로그인 기능 허용
+                // [로그인 기능]
                 .formLogin()
+                // 로그인 View 제공 (GET /user/login)
                 .loginPage("/user/login")
+                // 로그인 처리 (POST /user/login)
+                .loginProcessingUrl("/user/login")
+                // 로그인 처리 후 성공 시 URL
                 .defaultSuccessUrl("/")
+                // 로그인 처리 후 실패 시 URL
                 .failureUrl("/user/login?error")
                 .permitAll()
                 .and()
-                //로그아웃 기능 허용
+                // [로그아웃 기능]
                 .logout()
+                // 로그아웃 처리 URL
+                .logoutUrl("/user/logout")
                 .permitAll();
 
         return http.build();
